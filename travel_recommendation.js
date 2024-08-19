7344bf18cd7f786713539d0db54344823927c6a5
lineamregula <fr18west@gmail.com>
2024-08-19 14:51:30 -0700

    travel Reccomendations page

---
+++ travel_recommendation.js
@@ -0,0 +1,33 @@
+const btnSearch = document.getElementById('btnSearch');
+
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const beaches = condition.beaches.join(', ');
+          const temples = condition.temples.join(', ');
+          const countries = condition.countries;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${beaches}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${temples}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${countries}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
